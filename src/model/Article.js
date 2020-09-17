0010f109bcc79d3ec71e76c97e9dfde433137cac
Leandro Oliveira <leandroliveiradequeiroz@gmail.com>
2020-09-17 12:49:06 +0100

    Create backend - create title index for article search

--- src/model/Article.js
+++ src/model/Article.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const articleSchema = new Schema({
-  title: {type: String, required: true},
+  title: {type: String, index: {unique: false, background: false }, required: true},
   body: {type: String, required: true },
   author: {type: Schema.Types.ObjectId, ref: 'User', required: true}
 });
