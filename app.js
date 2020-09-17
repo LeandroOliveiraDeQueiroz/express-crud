eafae03951f7ed97f2ca65d3a8140eba88cbfad0
Leandro Oliveira <leandroliveiradequeiroz@gmail.com>
2020-09-17 14:06:51 +0100

    Create backend - remove comments and fix not found get article response

--- app.js
+++ app.js
@@ -10,11 +10,9 @@ const updateArticle = require('./src/endpoint/updateArticle.js');
 const getArticle = require('./src/endpoint/getArticle.js');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-//TODO estudar esse cors
 app.use(cors());
 app.use(bodyParser.json());
 
-//TODO Talvez criar arquivo de rotas
 app.post('/articles/random', createRandomsArticles);
 app.delete('/articles/all', deleteAll);
 app.put('/article/:id', updateArticle);
--- src/endpoint/getArticle.js
+++ src/endpoint/getArticle.js
@@ -11,7 +11,7 @@ function getArticle(req, res){
   }
 
   articleService.get(title).then((article)=>{
-    if (!article) {
+    if (!article || article.length === 0) {
       res.status(404).send({error: 'Article not found'});
     } else{
       res.status(200).send(article);
