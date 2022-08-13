e6613dba4054aa6fc4699d46b309b16a4348d1a8
LeandroOliveiraDeQueiroz <leandroliveiradequeiroz@gmail.com>
2022-08-13 13:51:49 -0300

    Transaction test

--- src/service/UserService.js
+++ src/service/UserService.js
@@ -1,51 +1,92 @@
-const User = require('../model/User');
+const User = require("../model/User");
 
 class UserService {
-
-  saveBulk(users) {
-    return new Promise((resolve, reject) => {
+  async saveBulk(users) {
+    return new Promise(async (resolve, reject) => {
       let promises = [];
 
-      users.forEach((user)=>{
-        promises.push(this.save(user));
-      });
+      const session = await User.startSession();
 
-      Promise.all(promises).then((savedUsers)=>{
-        resolve(savedUsers);
-      }).catch((e)=>{
-        console.log(e);
-        reject(e);
-      });
+      session.startTransaction();
+
+      // Use for and not forEach:
+      // https://stackoverflow.com/questions/70979039/node-js-foreach-loop-inside-try-catch-block
+      // save to save one, create can save a array sending a bunch of saves.
+      try {
+        for (var i = 0; i < users.length; i++) {
+          // if (i === 10) {
+          //   throw "Erro forçado";
+          // }
+          await User.create([users[i]], { session });
+        }
+
+        console.log("commit transacation");
+        await session.commitTransaction();
+      } catch (error) {
+        console.log("abort transacation", error);
+        await session.abortTransaction();
+      }
+      // await session.withTransaction(() => {
+      //   return User.create(users, { session });
+      // });
+
+      session.endSession();
+      resolve(null);
+
+      // Promise.all(promises).then((savedUsers)=>{
+      //   resolve(savedUsers);
+      // }).catch((e)=>{
+      //   console.log(e);
+      //   reject(e);
+      // });
     });
+
+    // return new Promise((resolve, reject) => {
+    //   let promises = [];
+
+    //   users.forEach((user)=>{
+    //     promises.push(this.save(user));
+    //   });
+
+    //   Promise.all(promises).then((savedUsers)=>{
+    //     resolve(savedUsers);
+    //   }).catch((e)=>{
+    //     console.log(e);
+    //     reject(e);
+    //   });
+    // });
   }
 
-  save(userData){
+  save(userData) {
     return new Promise((resolve, reject) => {
       const user = new User(userData);
-      
-      if(!user) {
+
+      if (!user) {
         reject();
       }
 
-      user.save().then(() => {
-        resolve(user);
-      }).catch((e) => {
-        reject(e);
-      });
-
+      user
+        .save()
+        .then(() => {
+          resolve(user);
+        })
+        .catch((e) => {
+          reject(e);
+        });
     });
   }
 
-  deleteAll(){
-    return new Promise((resolve, reject)=>{
-      User.deleteMany().then((res)=>{
-        resolve(res);
-      }).catch((e)=>{
-        reject(e);
-      });
+  deleteAll() {
+    return new Promise((resolve, reject) => {
+      User.deleteMany()
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((e) => {
+          reject(e);
+        });
     });
   }
-
 }
 
 module.exports = new UserService();
